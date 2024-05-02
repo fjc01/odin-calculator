e4834a039a1d3f023b83498a33650707cfeeab3d
fjc01 <165331935+fjc01@users.noreply.github.com>
2024-05-02 16:54:06 -0600

    Finished 4/7 steps

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-function add (a, b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
-function divide (a, b) {
+function divide(a, b) {
     return a / b;
 }
 
-let number1 = 0;
+let num1 = 0;
 let operator = undefined;
-let number2 = 0;
+let num2 = 0;
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Invalid Operation";
+    }
+}
+
+let result = operate (5, '/', 3);
+console.log(result);
