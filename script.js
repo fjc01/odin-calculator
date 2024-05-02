92f57fceb4e006cfe7e7cc9d1f47f489ea41c5d8
fjc01 <165331935+fjc01@users.noreply.github.com>
2024-05-02 16:36:55 -0600

    Halway there with there with the building steps

---
+++ script.js
@@ -0,0 +1,19 @@
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+let number1 = 0;
+let operator = undefined;
+let number2 = 0;
